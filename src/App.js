4ee1a3bdeb6604fea94bc8497b663344fd99cbb3
Bond252018 <nikoncort21@gmail.com>
2022-06-09 21:35:52 +0300

    delete link

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ function App() {
         <section className="hero-sections">
           <div className="container">                      
             <div className="home-background">                                                      
-                <div className="split">
-                  <div className="background-item-1"></div>
-                </div>                
-                <div className="split">
-                  <div className="background-item-2"></div>
-                </div>
-                <div className="split">
-                  <div className="background-item-3"></div>
-                </div>
-                <div className="split">
-                  <div className="background-item-4"></div>
-                </div>               
-              </div>     
-              <h1 className="history-war">ІСТОРІЇ ПРО ВІЙНУ</h1>               
+              <div className="split">
+                <div className="background-item-1"></div>
+              </div>                
+              <div className="split">
+                <div className="background-item-2"></div>
+              </div>
+              <div className="split">
+                <div className="background-item-3"></div>
+              </div>
+              <div className="split">
+                <div className="background-item-4"></div>
+              </div>               
+            </div>     
+            <h1 className="history-war">ІСТОРІЇ ПРО ВІЙНУ</h1>               
               <nav className="header-nav">
-                  <a href="#" className="stories-header active">Люди<span></span></a>
-                    <span className="nav-spacer"></span>
-                  <a className="countries-header" href="/countries">Історії<span></span></a>
-                    <span className="nav-spacer"></span>
-                  <a className="series-header" href="#">Війна<span></span></a>
-                </nav> 
-                <p>Історії людей, що втекли від війни</p>
+                <span className="bottom-hover  active">Люди</span>
+                  <span className="nav-spacer"></span>
+                <span className="bottom-hover" >Історії</span>
+                  <span className="nav-spacer"></span>
+                <span className="bottom-hover">Війна</span>
+              </nav> 
+              <p>Історії людей, що втекли від війни</p>
                 <div class="arrow">  
-                <a href="#yak2">                
-                  <svg x="0px" y="0px" viewBox="0 0 500 500"><path d="M111,187.4c-7.6,6.6-7.6,17.3,0,23.9l116.2,101.2c7.6,6.6,19.8,6.6,27.4,0l116.2-101.2 c3.8-3.3,5.7-7.6,5.7-11.9c0-4.3-1.9-8.6-5.7-11.9c-7.6-6.6-19.8-6.6-27.4,0l-102.5,89.3l-102.5-89.3 C130.8,180.9,118.6,180.9,111,187.4z"></path></svg>              
-                </a>
+                  <a href="#yak2">                
+                    <svg x="0px" y="0px" viewBox="0 0 500 500"><path d="M111,187.4c-7.6,6.6-7.6,17.3,0,23.9l116.2,101.2c7.6,6.6,19.8,6.6,27.4,0l116.2-101.2 c3.8-3.3,5.7-7.6,5.7-11.9c0-4.3-1.9-8.6-5.7-11.9c-7.6-6.6-19.8-6.6-27.4,0l-102.5,89.3l-102.5-89.3 C130.8,180.9,118.6,180.9,111,187.4z"></path></svg>              
+                  </a>
                 </div>                
-                </div>
-          </section>
+            </div>
+        </section>
           <section className="info-sections" >        
             <div className="container">   
               <div className="container-header" id="yak2"> 
