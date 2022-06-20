1d2b5011a56e65bbbcc56cb1e65d7c1d3cc3641f
Bond252018 <nikoncort21@gmail.com>
2022-06-20 19:56:45 +0300

    add p new size video

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ const App =() => {
         <Stories />
         <section className="footer">
           <div className="container">
-           <iframe width="560px" height="315px" src="https://www.youtube.com/embed/EEDFwUl08M8" title="YouTube video player"></iframe>
+            <div className="video-holder">              
+              <iframe width="100%" height="315px" src="https://www.youtube.com/embed/EEDFwUl08M8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
               {/* <p className="footer-text">Більше історій про Війну</p> 
                 <div className="social-footer">             
                 </div>  */}
-            <p>Хочеш розповісти свою історію?
-            Надсилай на пошту</p>
+            <p className="footer-text">Хочеш розповісти свою історію?
+              Надсилай на пошту</p>
             <p>strelchenko.t.v@gmail.com</p>
           </div> 
         </section>
